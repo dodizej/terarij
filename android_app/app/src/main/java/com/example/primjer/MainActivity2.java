3a831497dc48df39fbbbdd40d69aef0947e0b096
BigSvibba <144054238+BigSvibba@users.noreply.github.com>
2023-09-06 00:13:11 +0200

    AndroidStudio error fixed
    Code is fixed and working. It is important to check a box "Launcher" when creating activity

---
+++ android_app/app/src/main/java/com/example/primjer/MainActivity2.java
@@ -0,0 +1,78 @@
+package com.example.primjer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+    private Button pumpaON,pumpaOFF,difuzorOn,difuzorOff,SvjetloOn,SvjetloOff,SvjetlinaGore,SvjetlinaDolje;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        pumpaON=(Button) findViewById(R.id.pumpaON);
+        pumpaOFF=(Button) findViewById(R.id.pumpaOff);
+        difuzorOn=(Button) findViewById(R.id.difuzorON);
+        difuzorOff=(Button) findViewById(R.id.difuzorOff);
+        SvjetloOn=(Button) findViewById(R.id.SvjetloON);
+        SvjetloOff=(Button) findViewById(R.id.SvjetloOFF);
+        SvjetlinaDolje=(Button) findViewById(R.id.SvjetlinaDolje);
+        SvjetlinaGore=(Button) findViewById(R.id.SvjetlinaGore);
+        pumpaON.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        pumpaOFF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        difuzorOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        difuzorOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        SvjetloOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        SvjetloOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        SvjetlinaGore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        SvjetlinaDolje.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
+
+
+    }
+}
